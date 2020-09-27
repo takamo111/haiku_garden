f66cd90d05e26d98378240cb43a37a871c04c74f
yuhakare <iheuyo927@yahoo.co.jp>
2020-09-27 11:17:29 +0900

    S3-re2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,8 +8,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID']
-      aws_secret_access_key: ['AWS_IAM_ACCESS_KEY']
+      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
       region: 'ap-northeast-1'
 
     }
